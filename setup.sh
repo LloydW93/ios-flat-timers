2e190281ac77566ef549b2aab6a9837a86e2b401
Lloyd Wallis <lloyd.wallis@bbc.co.uk>
2020-05-08 01:08:54 +0100

    Setup script configures systemd too

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 ln -s `pwd`/onair-lights-off.service /etc/systemd/system/
 ln -s `pwd`/onair-lights-off.timer /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable onair-lights-off.timer
+systemctl start onair-lights-off.timer
 
